cee454e4e65cca5d8f6ff481afd285da4e53e13c
ej <ej30827@gmail.com>
2019-01-25 13:41:17 +0800

    homework2 重複訂票功能

--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -3,26 +3,26 @@ package com.train
 import java.util.*
 
 fun main(args: Array<String>) {
- val scanner = Scanner(System.`in`)
+        var totalTicket=0
+        val scanner = Scanner(System.`in`)
+    while (totalTicket!=-1) {
         println("請輸入需購買張數:")
-        val totalTicket = scanner.nextInt()
+        totalTicket = scanner.nextInt()
+        if (totalTicket==-1)
+            break
+
         println("請輸入來回票張數:")
         val roundTrip = scanner.nextInt()
         val tic = TicketKotlin(totalTicket, roundTrip)
 
-        println(
-            "Total tickets:" + tic.totalTickets + "\n Round-trip:" + tic.roundTrip +
-    "\n Total:" + tic.cost()
-        )
+        println("Total tickets:"+tic.totalTickets+ "\n Round-trip:" +tic.roundTrip+" \n Total:"+tic.cost())
+    }
 }
 
 class TicketKotlin(totalTickets : Int,roundTrip : Int){
 
-    var totalTickets=totalTickets;
-    var roundTrip=roundTrip;
-    fun cost(): Int{
-        var Total = roundTrip * 1800 + (totalTickets - roundTrip) * 1000
-        return Total
-    }
+    var totalTickets=totalTickets
+    var roundTrip=roundTrip
+    fun cost()=roundTrip * 1800 + (totalTickets - roundTrip) * 1000
 
 }
